912b98bbe7fb85d570ed727a83b8ddab02dc69f6
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-12 15:55:47 +1000

    Updated Expense Tracker React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
 
+// Components
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </div>
+  );
 }
 
 export default App;
