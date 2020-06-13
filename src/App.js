fd5a5a37735f8bcbd508e11607745fc6f44bdd25
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-13 14:15:24 +1000

    Updated AppReducer.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
+// Context
+import { GlobalProvider } from './context/GlobalState';
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -18,7 +21,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
